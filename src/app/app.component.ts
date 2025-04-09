36ecabdc7be48b4d9929c6ee5eef8c4908538ecc
GusSegura <gustavo.igemer@gmail.com>
2025-04-08 20:12:54 -0600

    carga del test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from "./task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [TaskListComponent],
 })
 export class AppComponent {
   title = 'angular-testing-project';
+  mensajerecibido :string[] = [];
+  constructor(){}
 
   addTask(task: string) {
-
+    this.mensajerecibido=this.getTasks();
   }
 
   deleteTask(task: string) {
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
@@ -6,15 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
+  @Input() mensaje:string[]=[];
+  @Output() nuevoMensaje=new EventEmitter<string>
 
+  tarea!: string;
   tasks: string[] = [];
 
   constructor() {
     console.log('TaskListComponent initialized');
-  }
+    
+    }
+  
 
   addTask(task: string) {
     console.log('Adding task:', task);
+    this.nuevoMensaje.emit(this.msg);
+  
   }
 
   updateTask(task: string) {
@@ -23,5 +30,8 @@ export class TaskListComponent {
 
   deleteTask(task: string) {
     console.log('Deleting task:', task);
+  }
+  showtask(tarea: string){
+    console.log(tarea);
   }
 }
